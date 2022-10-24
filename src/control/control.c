22a080e63a643ef17630c2700dbe2435b0960906
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2022-10-24 00:07:04 -0300

    implementing functions

--- src/control/control.c
+++ src/control/control.c
@@ -1,1 +1,17 @@
 #include "control.h"
+
+void Jz(byte* pc) {}
+
+void Jnz(byte* pc) {}
+
+void Jg(byte* pc) {}
+
+void Jl(byte* pc) {}
+
+void Jge(byte* pc) {}
+
+void Jle(byte* pc) {}
+
+void Jmp(byte* pc) {}
+
+void Hlt(byte* pc) {}
