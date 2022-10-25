b6fd10b3451bc808175e12a6306a3ebdb8b85823
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2022-10-24 23:14:12 -0300

    deleting useless headers

--- src/helpers/helpers.h
+++ src/helpers/helpers.h
@@ -12,9 +12,6 @@
 
 void run_program(Instruction* program, byte* mem, byte* acc, byte* pc, byte* stat);
 void process_instructions(Instruction* program, byte* mem, byte* acc, byte* pc, byte* stat);
-void process_control_instructions(Instruction instruction, byte* mem, byte* acc, byte* pc, byte* stat);
-void process_data_processing_instructions(Instruction instruction, byte* mem, byte* acc, byte* pc, byte* stat);
-void process_data_storage_instructions(Instruction instruction, byte* mem, byte* acc, byte* pc, byte* stat);
 void load_program(Instruction* program, char* file_name);
 
 #endif
