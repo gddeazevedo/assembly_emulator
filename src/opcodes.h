f88b1302fe1f6d0f675bb53fcf2203c1b325bc00
gddeazevedo <gabrielcapuerj@gmail.com>
2022-11-03 17:12:45 -0300

    identation

--- src/opcodes.h
+++ src/opcodes.h
@@ -3,23 +3,23 @@
 
 #define LOAD_MEM 0
 #define LOAD_VAL 1
-#define STORE 2
-#define ADD   3
-#define SUB   4
-#define MUL   5
-#define DIV   6
-#define INC   7
-#define DEC   8
-#define AND   9
-#define OR   10
-#define NOT  11
-#define JMP  12
-#define JZ   13
-#define JNZ  14
-#define JG   15
-#define JL   16
-#define JGE  17
-#define JLE  18
-#define HLT  19
+#define STORE    2
+#define ADD      3
+#define SUB      4
+#define MUL      5
+#define DIV      6
+#define INC      7
+#define DEC      8
+#define AND      9
+#define OR      10
+#define NOT     11
+#define JMP     12
+#define JZ      13
+#define JNZ     14
+#define JG      15
+#define JL      16
+#define JGE     17
+#define JLE     18
+#define HLT     19
 
 #endif
